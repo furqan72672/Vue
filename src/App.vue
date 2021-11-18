76c507ced423b179f26f3dd64a83bcc48d9a3ea6
furqan72672 <furqan72672@gmail.com>
2021-11-18 19:07:44 +0500

    auth-completed

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main class="align-center">
+      <router-link to="/signin">SignIn</router-link>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import SignIn from './views/SignIn.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+  //   SignIn,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import VueRouter from 'vue-router'
+import SignIn from './views/SignIn'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+const routes=[
+  {path:'/'},
+  {path:'/signin',component:SignIn}
+]
+
+const router=new VueRouter({
+  routes
+})
+
 new Vue({
-  render: h => h(App),
+  router:router,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
